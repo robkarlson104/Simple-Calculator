17e9204523686eb50f76a0bfff3ab450de66bf4c
robkarlson104 <robkarlson104@gmail.com>
2026-04-21 21:35:29 -0400

    Implement basic calculator with memory functionality
    Add a simple calculator that stores calculations and allows clearing memory.

--- calculator.py
+++ calculator.py
@@ -1,1 +1,15 @@
 
+user_input = input('Press x to stop\nEnter your calculations - ')
+calc_list = []
+while user_input != "x":
+    user_input = input('Enter your calculations')
+    if user_input == del:
+        clear_memory(calc_list)
+    else:
+    calc_list += user_input
+else:
+    print(calc_list)
+
+
+def clear_memory(calc_list):
+    return calc_list == calc.list.clear()
